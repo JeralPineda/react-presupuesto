97714e718285d1aa6936166eed7fd8d04ed69338
Jeral <jeral.cris@gmail.com>
2021-09-11 20:18:10 -0600

    Leyendo el presupuesto del usuario y guardarlo en el stste

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Pregunta = () => {
+   // definir el state
+   const [cantidad, guardarCantidad] = useState(0);
+
+   // funcion que lee el presupuesto
+   const definirPresupuesto = (e) => {
+      guardarCantidad(parseInt(e.target.value, 10));
+   };
+
+   //    Submit para definir el presupuesto
+   const agregarPresupuesto = (e) => {
+      e.preventDefault();
+
+      // validar
+
+      // si se pasa la validacion
+   };
+
    return (
       <>
          <h2>Coloca tu presupuesto</h2>
 
-         <form>
+         <form onSubmit={agregarPresupuesto}>
             <input
                //
                type='number'
                className='u-full-width'
                placeholder='Coloca tu presupuesto'
+               onChange={definirPresupuesto}
             />
 
             <input
