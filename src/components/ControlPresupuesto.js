f8b6308d751aab841b8061750d81b41617ea1817
Jeral <jeral.cris@gmail.com>
2021-09-12 23:09:49 -0600

    Cambiando el color del restante conforme disminuye

--- src/components/ControlPresupuesto.js
+++ src/components/ControlPresupuesto.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { revisarPresupuesto } from '../helpers';
 
 export const ControlPresupuesto = ({ presupuesto, restante }) => {
    return (
       <>
-         <div class='alert alert-primary'>Presupuesto: ${presupuesto}</div>
-         <div class='alert '>Restante: ${restante}</div>
+         <div className='alert alert-primary'>Presupuesto: ${presupuesto}</div>
+         <div className={revisarPresupuesto(presupuesto, restante)}>Restante: ${restante}</div>
       </>
    );
 };
